f5eb5e3d6e60073d4f21f125d65ed8d02105005e
Kateryna Sosonna <kateryna.sosonna@globallogic.com>
2017-07-09 20:01:30 +0300

    comment deleted

--- src/main/java/com/tradereport/MainClass.java
+++ src/main/java/com/tradereport/MainClass.java
@@ -6,7 +6,6 @@ import com.tradereport.domain.enumeration.Entity;
 import com.tradereport.service.TradeReportCalcService;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -23,7 +22,6 @@ public class MainClass {
                 new Trade(Entity.BAR, "S", 0.22, Currency.AED, LocalDate.of(2016, 1, 5), LocalDate.of(2016, 1, 7), 450, 150.05),
                 new Trade(Entity.BAR, "S", 0.22, Currency.USD, LocalDate.of(2017, 7, 11), LocalDate.of(2017, 7, 11), 600, 120.05),
                 new Trade(Entity.BAR, "S", 0.22, Currency.USD, LocalDate.MAX, LocalDate.of(2017, 7, 11), 50, 120.05)
-             //   new Trade(Entity.FOO, "B", 0.5, Currency.SGP, LocalDate.MAX, LocalDate.of(2017, 7, 7), 200, 100.25)// FRI
         );
 
         TradeReportCalcService tradeService = new TradeReportCalcService();
